869e1d77cdd8eed4570f5e75d7c839e24702f454
Oren <orennoyes@Administrators-MacBook-Pro.local>
2022-09-19 22:26:26 -0600

    commit tree 63f74d2fd891708f316ec236bfb1d415ecf99f60

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 239 ) + 1;
+const num3 = 20
+const num4 = 5
+const mod = num3 / num4
+const max = Math.max(3, 6, 9, 12, 15, 18, 20);
